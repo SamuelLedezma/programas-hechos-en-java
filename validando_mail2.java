885d2120387144ac056e09bdbf3eaa157751eca6
samuelLedezma <samuelrodrigoledezmajaldin@gmail.com>
2020-08-18 23:04:20 -0300

    estoy subiendo otro archivo java que valida mails

---
+++ validando_mail2.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class validando_mail2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int	arroba =0;
+		boolean punto = false;
+		String mail= JOptionPane.showInputDialog("ingrese mail");
+		
+		for(int i=0;i<mail.length();i++) {
+			
+			if(mail.charAt(i)=='@') {
+				arroba ++;
+				
+			}
+			if(mail.charAt(i)=='.') {
+				punto = true;
+			}
+		
+		}
+		if(arroba==1 && punto==true) {
+			System.out.println("es correcto");
+			
+		}
+		else {
+			System.out.println("es incorrecto");
+		}
+	}
+
+}
+	
